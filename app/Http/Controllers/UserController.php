a5d8fc0c2ddf857ccca0b17cc37181b224fd3dfe
lixiaode222 <1803118517@qq.com>
2018-09-10 17:10:56 +0800

    finfish register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -10,4 +11,28 @@ class UserController extends Controller
     public function create(){
         return view('user.create');
     }
+
+    //个人用户页面
+    public function show(User $user){
+        return view('user.show',compact('user'));
+    }
+
+    //处理用户注册逻辑
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+           'name' => $request->name,
+           'email' => $request->email,
+           'password' => bcrypt($request->name)
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show',[$user]);
+    }
+
 }
