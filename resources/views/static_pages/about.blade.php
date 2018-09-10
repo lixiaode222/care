61a5c6799a9ba59e55974519e882adcccce03f8e
lixiaode222 <1803118517@qq.com>
2018-09-10 15:33:16 +0800

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title','关于')
+
+@section('content')
+    <h1>关于页</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title','帮助')
+
+@section('content')
+    <h1>帮助页</h1>
+@stop
